feef7985133241c5e11622b0932d3eb629e7fbfe
Mitch LeBlanc <supermitch@gmail.com>
2015-09-24 21:44:34 -0700

    Add soup extraction of links in results page

--- craigschart/craigschart.py
+++ craigschart/craigschart.py
@@ -1,5 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_html():
+    r = requests.get('http://vancouver.craigslist.ca/search/cto?query=Expedition')
+    print(r.status_code)
+    print(r.text)
+    return r.text
 
 def main():
+    html = get_html()
+
+    soup = BeautifulSoup(html, 'lxml')
+    print(soup.prettify())
+    mydivs = soup.findAll('a', {'class': 'hdrlnk'})
+    for t in mydivs:
+        print(t)
+
     print('Hello, World.')
 
 if __name__ == '__main__':
