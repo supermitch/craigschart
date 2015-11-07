c2394eb70564dd8a4d7b969343fafd3868b23a81
Mitch LeBlanc <supermitch@gmail.com>
2015-11-06 21:53:36 -0800

    Fix axes titles and remove temp annotation on graph

--- craigschart/graph.py
+++ craigschart/graph.py
@@ -1,22 +1,20 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-def graph(points):
-    """ Generate Plotly graph. """
+
+def graph(points, search_string=""):
+    """ Generate Plotly graph and return URL. """
     data = [go.Scatter(
         x=[x for x, _ in points],
         y=[y for _, y in points],
         mode='markers'
     )]
 
-    layout = go.Layout(title="Expedition",
-                       xaxis={'title': "time"},
-                       annotations=[{'text': 'simple annotation',
-                                     'x': 0, 'xref': 'paper',
-                                     'y': 0, 'yref': 'paper'}]
+    layout = go.Layout(title="Results for '{}'".format(search_string),
+                       xaxis={'title': 'Odometer (km)'},
+                       yaxis={'title': 'Price ($)'},
     )
 
     figure = go.Figure(data=data, layout=layout)
-    url = py.plot(figure, share='private', filename='line-scatter')
-    return url
+    return py.plot(figure, share='private', filename='line-scatter')
 
